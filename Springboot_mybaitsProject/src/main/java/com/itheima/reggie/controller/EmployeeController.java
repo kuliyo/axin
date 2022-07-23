8418693568b55955ceb286e1fca454731e61d8ee
“aXin” <aXin6>
2022-07-23 08:53:38 +0800

    spring01

--- Springboot_mybaitsProject/src/main/java/com/itheima/reggie/controller/EmployeeController.java
+++ Springboot_mybaitsProject/src/main/java/com/itheima/reggie/controller/EmployeeController.java
@@ -59,4 +59,16 @@ public class EmployeeController {
         request.getSession().setAttribute("employee",emp.getId());
         return R.success(emp);
     }
+
+    /**
+     * 员工退出
+     * @param request
+     * @return
+     */
+    @PostMapping("/logout")
+    public R<String> logout(HttpServletRequest request){
+        //清除Session中保存的当前登录员工的id
+        request.getSession().removeAttribute("employee");
+        return R.success("退出成功");
+    }
 }
